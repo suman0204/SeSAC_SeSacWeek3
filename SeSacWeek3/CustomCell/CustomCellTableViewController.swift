86e173186c33c508821a77db37adf2e4570443a5
sumanhong <suman0204@naver.com>
2023-08-01 19:03:18 +0900

    Delete Cell, Dynamic Button

--- SeSacWeek3/CustomCell/CustomCellTableViewController.swift
+++ SeSacWeek3/CustomCell/CustomCellTableViewController.swift
@@ -11,13 +11,39 @@ import UIKit
 
 class CustomCellTableViewController: UITableViewController {
     
-    let todo = TodoCSInformation()
+    @IBOutlet var searchBar: UISearchBar!
+    
+    //observer
+    var todo = TodoCSInformation() {
+        didSet { // 변수가 달라짐을 감지!
+            print("DidSet이 뭘까..")
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.rowHeight = 70
+        tableView.rowHeight = 80
+        
+        searchBar.placeholder = "할 일을 입력해보세요"
+        searchBar.searchTextField.addTarget(self, action: #selector(searchBarReturnTapped), for: .editingDidEndOnExit)
 
+    }
+    
+    @objc func searchBarReturnTapped() {
+        //Todo 항목을
+        let data = TodoCS(main: searchBar.text!, sub: "23.08.01", like: false, done: false) //옵셔널 해제해주기
+        
+        //list에 추가
+//        todo.list.append(<#T##newElement: TodoCS##TodoCS#>) // 배열 뒤쪽에 추가
+        todo.list.insert(data, at: 0) // 배열 앞쪽에 추가
+        
+        //UX - seachbar에 입력된 내용 없애주기
+        searchBar.text = ""
+        
+        //갱신 - 데이터가 변경되었기 때문에
+//        tableView.reloadData()
     }
 
     //1.셀 갯수
@@ -31,18 +57,39 @@ class CustomCellTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier) as! CustomTableViewCell
         
         let row = todo.list[indexPath.row]
-        
-//        cell.mainTitleLabel.text = row.main
-//        cell.subTitleLabel.text = row.sub
-        
+                
         cell.configureCell(row: row)
         
+        //IBAction 연결하면 버튼을 누르면 다른 셀들도 일괄 적용되기 때문에 코드로 구현
+        cell.likeButton.tag = indexPath.row
+        cell.likeButton.addTarget(self, action: #selector(likeButtonClicked), for: .touchUpInside)
         
         return cell
     }
     
+    
+    @objc func likeButtonClicked(_ sender: UIButton) {
+        print("clicked \(sender.tag)")
+        
+        todo.list[sender.tag].like.toggle()
+        
+//        tableView.reloadData()
+        
+    }
+    
+    //3. 셀 선택
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath)
+        
+    }
+    
+    //삭제
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        //제거 -> 갱신
+        todo.list.remove(at: indexPath.row)
+        
+//        tableView.reloadData()
     }
     
 }
